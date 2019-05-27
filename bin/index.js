702e8afd3c18f9851f386183f82aed26b6ba6504
Reinaldo Schiehll <rn.schiehll@gmail.com>
2019-05-27 18:27:47 -0300

    Add init command to cli

--- bin/index.js
+++ bin/index.js
@@ -3,16 +3,45 @@
 
 var _path = _interopRequireDefault(require("path"));
 
+var _fs = _interopRequireDefault(require("fs"));
+
 var _commander = _interopRequireDefault(require("commander"));
 
 var _shelljs = _interopRequireDefault(require("shelljs"));
+
+var _enquirer = require("enquirer");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const rootPath = _path.default.resolve(__dirname, '../');
 
-_commander.default.command('init').description('initialize kerbs').action(() => {
-  console.log('init!');
+_commander.default.command('init').description('initialize kerbs').action(async () => {
+  const response = await (0, _enquirer.prompt)({
+    type: 'input',
+    name: 'PROJECT_NAME',
+    message: `What's the project name?`,
+    initial: 'Unnamed Project'
+  });
+
+  const kerbsPath = _path.default.resolve(process.cwd(), '.kerbs');
+
+  const configPath = _path.default.resolve(kerbsPath, '.kerbsrc.json');
+
+  if (!_fs.default.existsSync(kerbsPath)) {
+    _fs.default.mkdirSync(kerbsPath);
+  }
+
+  if (_fs.default.existsSync(configPath)) {
+    console.log('.kerbs/.kerbsrc.json already exists');
+  } else {
+    _fs.default.writeFileSync(configPath, JSON.stringify({
+      name: response.PROJECT_NAME
+    }, null, 2), 'utf8');
+
+    _shelljs.default.cp('-r', _path.default.resolve(__dirname, '../src/templates/default/*'), kerbsPath);
+
+    _shelljs.default.touch('-c', _fs.default.readdirSync(kerbsPath));
+  }
 });
 
 _commander.default.command('dev').description('start dev server').action(() => {
@@ -26,3 +55,7 @@ _commander.default.command('dev').description('start dev server').action(() => {
 });
 
 _commander.default.parse(process.argv);
+
+if (_commander.default.args.length === 0) {
+  _commander.default.help();
+}
--- cli/index.js
+++ cli/index.js
@@ -1,16 +1,47 @@
 #!/usr/bin/env node
 
 import path from 'path'
+import fs from 'fs'
 import program from 'commander'
 import shell from 'shelljs'
+import { prompt } from 'enquirer'
 
 const rootPath = path.resolve(__dirname, '../')
 
 program
   .command('init')
   .description('initialize kerbs')
-  .action(() => {
-    console.log('init!')
+  .action(async () => {
+    const response = await prompt({
+      type: 'input',
+      name: 'PROJECT_NAME',
+      message: `What's the project name?`,
+      initial: 'Unnamed Project'
+    })
+
+    const kerbsPath = path.resolve(process.cwd(), '.kerbs')
+    const configPath = path.resolve(kerbsPath, '.kerbsrc.json')
+
+    if (!fs.existsSync(kerbsPath)) {
+      fs.mkdirSync(kerbsPath)
+    }
+
+    if (fs.existsSync(configPath)) {
+      console.log('.kerbs/.kerbsrc.json already exists')
+    } else {
+      fs.writeFileSync(
+        configPath,
+        JSON.stringify({ name: response.PROJECT_NAME }, null, 2),
+        'utf8'
+      )
+
+      shell.cp(
+        '-r',
+        path.resolve(__dirname, '../src/templates/default/*'),
+        kerbsPath
+      )
+      shell.touch('-c', fs.readdirSync(kerbsPath))
+    }
   })
 
 program
@@ -31,3 +62,7 @@ program
   })
 
 program.parse(process.argv)
+
+if (program.args.length === 0) {
+  program.help()
+}
