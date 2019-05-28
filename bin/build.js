c9b5a1aba58a548ac3c4195dce33f64341343a2c
Reinaldo Schiehll <rn.schiehll@gmail.com>
2019-05-28 19:27:41 -0300

    Add redirects file to build folder

--- bin/build.js
+++ bin/build.js
@@ -7,6 +7,8 @@ exports.default = void 0;
 
 var _path = _interopRequireDefault(require("path"));
 
+var _fs = _interopRequireDefault(require("fs"));
+
 var _shelljs = _interopRequireDefault(require("shelljs"));
 
 var _chalk = _interopRequireDefault(require("chalk"));
@@ -18,13 +20,17 @@ var _default = () => {
 
   const webpackConfig = _path.default.resolve(__dirname, '../webpack/webpack.config.js');
 
+  const kerbsPublic = _path.default.resolve(process.cwd(), 'kerbs_public');
+
   console.log(_chalk.default.cyan('Building kerbs...\n'));
 
-  _shelljs.default.rm('-rf', _path.default.resolve(process.cwd(), 'kerbs_public'));
+  _shelljs.default.rm('-rf', kerbsPublic);
 
   const code = _shelljs.default.exec(`node ${webpackExec} --mode production --config ${webpackConfig}`).code;
 
   if (code === 0) {
+    _fs.default.writeFileSync(`${kerbsPublic}/_redirects`, '/* /index.html 200');
+
     console.log(_chalk.default.green('\nSuccessfully built kerbs! Check kerbs_public folder.'));
   }
 };
--- cli/build.js
+++ cli/build.js
@@ -1,19 +1,23 @@
 import path from 'path'
+import fs from 'fs'
 import shell from 'shelljs'
 import chalk from 'chalk'
 
 export default () => {
   const webpackExec = path.resolve(__dirname, '../node_modules/.bin/webpack')
   const webpackConfig = path.resolve(__dirname, '../webpack/webpack.config.js')
+  const kerbsPublic = path.resolve(process.cwd(), 'kerbs_public')
 
   console.log(chalk.cyan('Building kerbs...\n'))
 
-  shell.rm('-rf', path.resolve(process.cwd(), 'kerbs_public'))
+  shell.rm('-rf', kerbsPublic)
   const code = shell.exec(
     `node ${webpackExec} --mode production --config ${webpackConfig}`
   ).code
 
   if (code === 0) {
+    fs.writeFileSync(`${kerbsPublic}/_redirects`, '/* /index.html 200')
+
     console.log(
       chalk.green('\nSuccessfully built kerbs! Check kerbs_public folder.')
     )
